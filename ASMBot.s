f5a60a4c8d66816be7cb45eba90c60147221157b
Detegr <detegr@gmail.com>
2012-08-07 20:06:49 +0300

    Connection

--- ASMBot.s
+++ ASMBot.s
@@ -19,7 +19,7 @@ setup_socket:
 
 	movl $102, %eax # sys_socketcall
 	movl $1,   %ebx # sys_socket
-	leal -12(%ebp), %ecx
+	leal -12(%ebp), %ecx # args ptr
 	int $0x80
 	
 	cmp $0, %eax
@@ -30,9 +30,37 @@ setup_socket:
 	leave
 	ret
 
+/*
+   struct sockaddr_in:
+   short sin_family (AF_INET)
+   unsigned short sin_port
+   unsigned long s_addr (inet_pton())
+   char[8] sin_zero
+
+   size: 16 bytes
+*/
 connect:
 	pushl %ebp
 	movl %esp, %ebp
+	subl $32, %esp # 26 used, aligned by 16
+
+	# build sockaddr_in
+	movw $2, -20(%ebp) # af_inet
+	movw $2842, -18(%ebp) # htons(6667) == 2842
+	movl $3568077907, -16(%ebp) # irc.quakenet.org
+
+	movl %eax, -12(%ebp) # fd
+	leal -20(%ebp), %edx # move sockaddr_in ptr via edx
+	movl %edx, -8(%ebp) # sockaddr_in ptr
+	movl $16, -4(%ebp) # addrlen
+
+	movl $102, %eax # sys_socketcall
+	movl $3, %ebx # sys_connect
+	leal -12(%ebp), %ecx # args ptr
+	int $0x80
+
+	cmp $0, %eax
+	jne fail
 
 	leave
 	ret
